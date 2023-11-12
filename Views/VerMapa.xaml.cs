19de73bcf62e0073dd8090fbbcdfb0864ea69731
l_alv <l_alv@DESKTOP-42DI0BD>
2023-11-11 21:12:35 -0600

    Agregar archivos de proyecto.

---
+++ Views/VerMapa.xaml.cs
@@ -0,0 +1,55 @@
+using Ejercicio2_1.Models;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+using Microsoft.Maui.Devices.Sensors;
+
+namespace Ejercicio2_1.Views
+{
+    public partial class VerMapa : ContentPage
+    {
+        Countries PaisSeleccionado;
+
+        public VerMapa(Countries country)
+        {
+            PaisSeleccionado = country;
+            InitializeComponent();
+            loadConfiguration(); // Asegúrate de llamar a loadConfiguration al inicio
+        }
+
+        private void loadConfiguration()
+        {
+            // Limpiar marcadores existentes
+            mapa.Pins.Clear();
+
+            if (PaisSeleccionado != null && PaisSeleccionado.latlng != null && PaisSeleccionado.latlng.Count >= 2)
+            {
+                var pin = new Pin()
+                {
+                    Type = PinType.SavedPin,
+                    Location = new Location(PaisSeleccionado.latlng[0], PaisSeleccionado.latlng[1]),
+                    Label = PaisSeleccionado.NameCountry.official,
+                    Address = "Capital: " + PaisSeleccionado.capital
+                };
+
+                mapa.Pins.Add(pin);
+                mapa.MoveToRegion(MapSpan.FromCenterAndRadius(new Location(PaisSeleccionado.latlng[0], PaisSeleccionado.latlng[1]), Distance.FromKilometers(41)));
+            }
+            else
+            {
+                Console.WriteLine("Datos de ubicación no válidos.");
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            loadConfiguration();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            mapa.Pins.Clear();
+        }
+    }
+}
